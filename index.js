a5c0823a9247c460b019ea5a0fe045f95f99886b
Sonechka M <s212288s@gmail.com>
2020-02-01 19:39:06 -0500

    getting prompts and creating user objects with classes

--- index.js
+++ index.js
@@ -1,19 +1,74 @@
-// ==== manager info:
-// name 
-// id 
-// email
-// office number
+const inquirer = require("inquirer");
+const Manager = require("./classes/manager.js");
+const fs = require('fs');
+const path = require("path");
 
-// ==== which type of team member would you like to add?
-// engeneer or intern or no more 
-// they describe 
-// i offer again
-
-// ==== make arrays for each team number, push members into arrays 
-
-// if they choose intern, ask infor about intern 
-// if the input is not no more keep asking questions
-
-// when i'm done building my team it's rendered 
-
-// make sure to keep asking question until they choose no more members 
+// ===== GET MANAGER'S INFO
+inquirer.prompt([
+    {
+      type: "input",
+      name: "manager",
+      message: "What is the team manager's name?"
+    },
+    {
+      type: "input",
+      name: "id",
+      message: "What is the team manager's id?"
+    },
+    {
+      type: "input",
+      name: "email",
+      message: "What is the team manager's email?"
+    },
+    {
+      type: "input",
+      name: "officeNumber",
+      message: "What is the team manager's office number?"
+    },
+    {
+      type: "checkbox",
+      message: "Which type of team member would you like to add?",
+      name: "member",
+      choices: [
+        "engineer", 
+        "intern", 
+        "team is complete", 
+      ]
+    }
+  ])
+  // ========= IF CHOICE IS ENGINEER GET ENGINEER INFO
+    .then((team) => {
+    const manager = new Manager(team.manager, team.id, team.email, team.officeNumber)
+    console.log(manager)
+    // console.log(team.member[0]);
+    // console.log("next team member", team.member)
+    if (team.member[0] === "engineer") {
+      inquirer.prompt([
+      {
+        type: "input",
+        name: "engineer",
+        message: "What is the engineer's name?"
+      },
+      {
+        type: "input",
+        name: "engineerId",
+        message: "What is the engineer's id"
+      },
+      {
+        type: "input",
+        name: "engineerEmail",
+        message: "What is the engineer's email?"
+      },
+      {
+          type: "input",
+          name: "engineerGitHubUsername",
+          message: "What is the engineer's GitHub username?"
+        }
+      
+    ])
+    .then((data) => {
+      console.log("engineer", data)
+    })
+    }
+    
+  });
---
+++ pseudo.js
@@ -0,0 +1,19 @@
+// ==== manager info:
+// name 
+// id 
+// email
+// office number
+
+// ==== which type of team member would you like to add?
+// engeneer or intern or no more 
+// they describe 
+// i offer again
+
+// ==== make arrays for each team number, push members into arrays 
+
+// if they choose intern, ask infor about intern 
+// if the input is not no more keep asking questions
+
+// when i'm done building my team it's rendered 
+
+// make sure to keep asking question until they choose no more members 
